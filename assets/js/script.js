b3f4cba93ed3f8bb73aad930695455211c07fb06
Holly Williams <hollyyeh12@gmail.com>
2023-01-31 15:54:16 +0000

    add java for random number selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,33 @@
-document.addEventListener("DOMContentLoaded", function(){
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
-    
+
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            if (this.getAttribrute("data-type") === "submit") {
-                alert("You Clicked Submit!");
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked Submit!");
             } else {
-                let gameType = this.getAttribrute("data-type");
-                alert("You Clicked ${gametype}");
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
             }
         })
-        
     }
 
 })
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame() {
 
-
-
-
-function runGame(){
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
 }
+
 function checkAnswer(){
 
 }
