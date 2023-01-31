1b4b733d42e6ae54986c72e86ebef46d524e5f5f
Holly Williams <hollyyeh12@gmail.com>
2023-01-31 00:22:00 +0000

    add javascript to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,42 @@
-function runGame()
-checkAnswer()
-calculateCorrectAnswer()
-incrementScore()
-incrementWrongAnswer()
-displayAdditionAnswer()
-displaySubtractAnswer()
-displayMultiplyAnswer()
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if(this.getAttribrute(data-type) === submit) {
+                alert("You Clicked Submit!");
+            } else {
+                let gameType = this.getAttribrute("data-type");
+                alert("You Clicked ${gametype}";
+            }
+        })
+        
+    }
+
+})
+
+
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionAnswer(){
+
+}
+function displaySubtractAnswer(){
+
+}
+function displayMultiplyAnswer(){
+
+}
